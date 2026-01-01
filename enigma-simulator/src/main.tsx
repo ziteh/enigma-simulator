2fb829ce9a1a67c78e8e67f52985cad50e818622
ZiTe <honmonoh@gmail.com>
2026-01-01 23:04:40 +0800

    feat: add dark theme

--- enigma-simulator/src/main.tsx
+++ enigma-simulator/src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
