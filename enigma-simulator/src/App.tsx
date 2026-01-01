76c5f32d694c4da73dc2ec61c9c4231f201b73ad
ZiTe <honmonoh@gmail.com>
2026-01-01 23:12:25 +0800

    style: just format

--- enigma-simulator/src/App.tsx
+++ enigma-simulator/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
--- enigma-simulator/src/main.tsx
+++ enigma-simulator/src/main.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from '@/components/theme-provider'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from "@/components/theme-provider";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
   </StrictMode>,
-)
+);
